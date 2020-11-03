36578bb438fd14111334852582f01e25cc6d6f90
alexConkey <aconkey20@stuy.edu>
2020-11-03 17:05:25 -0500

    constructor, size(), and add() done

---
+++ SuperArray.java
@@ -0,0 +1,15 @@
+public class SuperArray{
+  private String[] data;
+  private int size;
+  public SuperArray(){
+    data = new String[10];
+    size = 0;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(String element){
+    data[size] = element;
+    size++;
+  }
+}
