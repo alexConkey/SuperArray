816a5c93e0fccbf26104f3e8c31cbb1ef6591f02
alexConkey <aconkey20@stuy.edu>
2020-11-03 17:09:14 -0500

    get() and set() done

--- SuperArray.java
+++ SuperArray.java
@@ -12,4 +12,10 @@ public class SuperArray{
     data[size] = element;
     size++;
   }
+  public String get(int index){
+    return data[index];
+  }
+  public String set(int index, String element){
+    data[index] = element;
+  }
 }
