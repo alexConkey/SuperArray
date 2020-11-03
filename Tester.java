1f34abbf2d7be1a7b941816bfae8891ed49d9db7
alexConkey <aconkey20@stuy.edu>
2020-11-03 18:14:56 -0500

    Everything done

--- Tester.java
+++ Tester.java
@@ -2,5 +2,11 @@ public class Tester {
   public static void main(String[] args) {
     SuperArray mySuperArray = new SuperArray();
     System.out.println(mySuperArray.size());
+    for (int i = 0; i < 15; i++){
+      mySuperArray.add("Hi"+Integer.toString(i));
+    }
+    System.out.println(mySuperArray.get(12));
+    mySuperArray.set(12, "Not Hi");
+    System.out.println(mySuperArray.get(12));
   }
 }
