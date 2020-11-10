52158daeba80f588c01257dde29d62c40f7e4c3d
alexConkey <aconkey20@stuy.edu>
2020-11-10 10:41:58 -0500

    zip

--- Demo.java
+++ Demo.java
@@ -11,13 +11,33 @@ public class Demo{
     for (int i = 0; i < removals; i++){
       s.remove(i);
     }
+    public static SuperArray zip(SuperArray a, SuperArray b){
+      SuperArray finalSA = new SuperArray();
+      if (a.size()>b.size()){
+        for(int i = 0; i<b.size(); i++){
+          finalSA.add(a.get(i));
+          finalSA.add(b.get(i));
+        }
+        for(int i = b.size(); i<a.size(); i++){
+          finalSa.add(a.get(i));
+        }
+      }else{
+        for(int i = 0; i<a.size(); i++){
+          finalSA.add(a.get(i));
+          finalSA.add(b.get(i));
+        }
+        for(int i = a.size(); i<b.size(); i++){
+          finalSa.add(b.get(i));
+        }
+      }
+    }
   }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
-    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-    words.add("una");    words.add("ebi");     words.add("toro");
+    words.add("una");
 
     System.out.println(words);
     removeDuplicates(words);
