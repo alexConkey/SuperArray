541fccec42733293f0aef7303dc6fcd0fe8764c8
alexConkey <aconkey20@stuy.edu>
2020-11-10 10:26:14 -0500

    equals

--- SuperArray.java
+++ SuperArray.java
@@ -109,4 +109,15 @@ public class SuperArray{
     }
     return -1;
   }
+  public boolean equals(SuperArray other){
+    if size = other.size(){
+      for (int i = 0; i<size; i++){
+        if (data[i] != other.get(i)){
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
 }
